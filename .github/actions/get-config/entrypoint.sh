6793044ee1042452af546fe47904ae3032b7f666
Yoav Dobrin <yodobrin@microsoft.com>
2023-04-18 17:22:49 +0300

    pipeline and compote

--- .github/actions/get-config/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-FILE_NAME=$INPUT_FILE_NAME
-
-python -c "import yaml; data = yaml.safe_load(open('$FILE_NAME')); print(data)"
-
-echo ::set-output name=config::$(python -c "import yaml; data = yaml.safe_load(open('$FILE_NAME')); print(data)")
